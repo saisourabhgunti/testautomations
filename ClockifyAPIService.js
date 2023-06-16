8d7d84078b8479eb13298c9dc2709b34a0ece4aa
saisourabhgunti <110903448+saisourabhgunti@users.noreply.github.com>
2023-06-16 11:22:25 +0530

    Removed cron part

--- ClockifyAPIService.js
+++ ClockifyAPIService.js
@@ -1,53 +1,41 @@
 const https = require('https');
-const cron = require('node-cron');
-
-const apiKey = 'ZjQ3NTNhMDgtNWNjYS00YzhhLWI1Y2YtODA5YmJmMWMwZWEx';
-
-const headers = {
-    'Content-Type': 'application/json',
-    'X-Api-Key': apiKey,
-};
-
-const currentDate = new Date().toISOString().split('T')[0]; // Get current date in "YYYY-MM-DD" format
-
-const body = JSON.stringify({
-    start: `${currentDate}T16:30:00.000Z`, // Set the time to 6 PM in UTC
-    end: `${currentDate}T12:30:00.000Z`,
-    billable: true,
-    description: 'CW Training',
-    projectId: '64805a0ca2fe384cb7ff1eb5',
-    taskId: null,
-    tagIds: null,
-    customFields: [],
-});
 
 const options = {
     hostname: 'global.api.clockify.me',
     path: '/workspaces/5eda2300fe14aa072a454f01/timeEntries/full',
     method: 'POST',
-    headers: headers,
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Api-Key': 'ZjQ3NTNhMDgtNWNjYS00YzhhLWI1Y2YtODA5YmJmMWMwZWEx'
+    }
 };
 
-// Schedule the task to run at 6 PM every day 18
-cron.schedule('25 11 * * *', () => {
-    const req = https.request(options, (res) => {
-        let responseData = '';
+const req = https.request(options, res => {
+    let data = '';
 
-        res.on('data', (chunk) => {
-            responseData += chunk;
-        });
-
-        res.on('end', () => {
-            console.log('API call successful');
-            console.log('Response:', JSON.parse(responseData));
-        });
+    res.on('data', chunk => {
+        data += chunk;
     });
 
-    req.on('error', (error) => {
-        console.error('API call failed');
-        console.error('Error:', error.message);
+    res.on('end', () => {
+        console.log('API call successful:', data);
     });
-
-    req.write(body);
-    req.end();
 });
+
+req.on('error', error => {
+    console.error('API call error:', error);
+});
+
+const payload = JSON.stringify({
+    start: new Date().toISOString(),
+    end: new Date().toISOString(),
+    billable: true,
+    description: 'CW Training',
+    projectId: '64805a0ca2fe384cb7ff1eb5',
+    taskId: null,
+    tagIds: null,
+    customFields: []
+});
+
+req.write(payload);
+req.end();
